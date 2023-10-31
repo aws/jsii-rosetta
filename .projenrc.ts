fe185c5adbf41390d346688a32f10225fb960907
Momo Kornher <kornherm@amazon.co.uk>
2023-10-31 12:09:55 +0000

    chore: better dependency upgrades for v5.2 (#301)
    Same as https://github.com/aws/jsii-rosetta/pull/300
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,7 @@ import { versionMajorMinor } from 'typescript';
 import { BuildWorkflow } from './projenrc/build-workflow';
 import { ReleaseWorkflow } from './projenrc/release';
 import { SUPPORT_POLICY, SupportPolicy } from './projenrc/support';
-import { UpgradeDependencies } from './projenrc/upgrade-dependencies';
+import { JsiiDependencyUpgrades } from './projenrc/upgrade-dependencies';
 
 // This should be '0' for new version lines
 // However it might be required to depend on a version with a specific feature or bug-fix
@@ -128,20 +128,7 @@ const project = new typescript.TypeScriptProject({
   JsonPatch.add('/on/merge_group', {}),
 );
 
-new UpgradeDependencies(project, {
-  workflowOptions: {
-    branches: [
-      'main',
-      ...Object.entries(SUPPORT_POLICY.maintenance).flatMap(([version, until]) => {
-        if (Date.now() > until.getTime()) {
-          return [];
-        }
-        return [`maintenance/v${version}`];
-      }),
-    ],
-    labels: ['auto-approve'],
-  },
-});
+new JsiiDependencyUpgrades(project);
 
 // VSCode will look at the "closest" file named "tsconfig.json" when deciding on which config to use
 // for a given TypeScript file with the TypeScript language server. In order to make this "seamless"
