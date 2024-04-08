b7f8aa5f71fb354a378c3f3b5aafda04789edf4a
Eli Polonsky <Eli.polonsky@gmail.com>
2024-04-08 12:45:09 +0300

    feat: TypeScript 5.4 (#1110)
    [TypeScript 5.4 has been
    released](https://devblogs.microsoft.com/typescript/announcing-typescript-5-4/).
    This moves jsii-rosetta 5.3 into maintenance mode.
    
    > Requires https://github.com/aws/jsii-compiler/pull/855 to merge and
    publish before
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0
    
    ---------
    
    Signed-off-by: Eli Polonsky <Eli.polonsky@gmail.com>
    Signed-off-by: Momo Kornher <mail@moritzkornher.de>
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: Momo Kornher <kornherm@amazon.co.uk>
    Co-authored-by: github-actions <github-actions@github.com>

--- projenrc/support.ts
+++ projenrc/support.ts
@@ -10,14 +10,15 @@ interface ReleasesDocument {
 }
 
 export const SUPPORT_POLICY: ReleasesDocument = {
-  current: '5.3',
+  current: '5.4',
   // Define a different patch version here if a specific feature or bug-fix
-  currentMinVersionNumber: '5.3.0',
+  currentMinVersionNumber: '5.4.0',
   maintenance: {
     // version: End-of-support date
     '5.0': new Date('2024-01-31'),
     '5.1': new Date('2024-04-30'),
     '5.2': new Date('2024-06-30'),
+    '5.3': new Date('2024-10-15'),
   },
 };
 
