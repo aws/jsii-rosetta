ca20e3199551849a86bc4afccb6082f4bc5d283a
Momo Kornher <kornherm@amazon.co.uk>
2024-04-08 10:27:03 +0100

    Update projenrc/support.ts
    Signed-off-by: Momo Kornher <mail@moritzkornher.de>

--- projenrc/support.ts
+++ projenrc/support.ts
@@ -18,7 +18,7 @@ export const SUPPORT_POLICY: ReleasesDocument = {
     '5.0': new Date('2024-01-31'),
     '5.1': new Date('2024-04-30'),
     '5.2': new Date('2024-06-30'),
-    '5.3': new Date('2024-10-07'),
+    '5.3': new Date('2024-10-15'),
   },
 };
 
