8cb9f8ddefd01489075da79414177e4889421950
Momo Kornher <kornherm@amazon.co.uk>
2024-11-28 12:01:40 +0000

    feat: TypeScript 5.7 (#2194)
    Add support for jsii 5.7 and TypeScript 5.7
    
    https://devblogs.microsoft.com/typescript/announcing-typescript-5-7/
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -12,23 +12,24 @@ import { JsiiDependencyUpgrades } from './projenrc/upgrade-dependencies';
  *  2. Fork the current `main` to a maintenance branch:
  *     `git switch main && git fetch --all && git pull`
  *     `git push origin main:maintenance/vX.Y` (X.Y is the TS version that is about to be replaced by a new release)
- *  3. Add a branch protection rule for the new maintenance branch
- *     Copy the settings from the most recent maintenance branch.
- *  4. Edit `support.ts`, maintenance EOL date for the current version is 6 months from
+ *  3. Add the just created branch as a target to the "current" ruleset.
+ *  4. Create a new branch for the new version: `git switch --create feat/tsX.Y`
+ *  5. Edit `projenrc/support.ts`, maintenance EOL date for the current version is 6 months from
  *     today (round up to the mid-point or end of month), make the new version current.
  *     Also update `currentMinVersionNumber`.
- *  5. Update `minNodeVersion` to the oldest LTS version of Node (i.e. dropping support for EOL versions of Node)
- *  6. Update the version list in the README (remember to remove EOS versions)
- *  7. `npx projen`
- *  8. `npx build` and fix any issues that come up
- *  9. Create a PR, with title "feat: TypeScript X.Y"
- * 10. Note that merging the PR doesn't trigger a release. Releases are performed on a weekly schedule.
+ *  6. Update `minNodeVersion` to the oldest LTS version of Node (i.e. dropping support for EOL versions of Node)
+ *  7. Update the version list in the README (remember to remove EOS versions)
+ *  8. If any versions dropped into EOS, add the respective branch as a target to the "end-of-support" ruleset.
+ *  9. `npx projen`
+ *  10. `npx build` and fix any issues that come up
+ * 11. Create a PR, with title "feat: TypeScript X.Y"
+ * 12. Note that merging the PR doesn't trigger a release. Releases are performed on a weekly schedule.
  *     You should manually create a release by triggering this workflow:
  *     https://github.com/aws/jsii-rosetta/actions/workflows/auto-tag-releases.yml
- * 11. Add support for the new rosetta version line to `jsii-pacmak`.
+ * 13. Add support for the new rosetta version line to `jsii-pacmak`.
  *     See https://github.com/aws/jsii/blob/main/CONTRIBUTING.md#support-for-new-jsii-rosetta-versions
- * 12. Add support for the new rosetta version line in `jsii-docgen` (have a look at RosettaPeerDependency in projenrc.ts).
- * 13. Once jsii-docgen is released, add support for the new jsii version line to projen.
+ * 14. Add support for the new rosetta version line in `jsii-docgen` (have a look at RosettaPeerDependency in projenrc.ts).
+ * 15. Once jsii-docgen is released, add support for the new jsii version line to projen.
  *     Example: https://github.com/projen/projen/pull/3805
  */
 
